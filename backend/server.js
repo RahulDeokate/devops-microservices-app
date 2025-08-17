17b8d87014586ac9222fad91f926565c6762c299
Rahul Deokate <rahuldeokate47666@gmail.com>
2025-08-17 13:34:53 +0200

    Added backend logic

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-
+const express = require("express");
+const app = express();
+const PORT = 3000;
+ 
+app.use(express.json());
+ 
+app.get("/api/profile", (req, res) => {
+  res.json({
+    name: "Rahul Deokate",
+    role: "DevOps Enthusiast 🚀"
+  });
+});
+ 
+app.get("/api/health", (req, res) => {
+  res.json({ status: "Backend is running ✅" });
+});
+ 
+app.listen(PORT, () => {
+  console.log(`Backend service running on port ${PORT}`);
+});
